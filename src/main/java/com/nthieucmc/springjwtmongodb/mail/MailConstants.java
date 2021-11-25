2bd74775a0697365b0e6e50fe9b145dbc7e94222
Nguyen Trung Hieu <36031709+Hieungx@users.noreply.github.com>
2021-11-25 08:32:34 +0700

    Hide password

--- src/main/java/com/nthieucmc/springjwtmongodb/mail/MailConstants.java
+++ src/main/java/com/nthieucmc/springjwtmongodb/mail/MailConstants.java
@@ -5,7 +5,7 @@ public class MailConstants {
     public static final String MY_EMAIL = "hvt311097@gmail.com";
 
     // Replace password!!
-    public static final String MY_PASSWORD = "truong311097";
+    public static final String MY_PASSWORD = "********";
 
     // And receiver!
     public static final String FRIEND_EMAIL = "hieuu99@gmail.com";
